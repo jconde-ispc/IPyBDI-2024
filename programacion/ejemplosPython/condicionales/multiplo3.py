4a97ac36fa0c4f3d918b27faf998c5cb7a4c8fae
jconde-ispc <julianconde.ispc@gmail.com>
2024-05-07 16:09:21 -0300

    agregando el ejemplo de multiplo de 3

--- programacion/ejemplosPython/condicionales/multiplo3.py
+++ programacion/ejemplosPython/condicionales/multiplo3.py
@@ -1,1 +1,12 @@
 # https://colab.research.google.com/drive/1YY7crmvqZdDpAgBrztoqqPnKQopItllT?usp=sharing
+
+# Declaracion de variables:
+int_Numero = 0
+
+# Solicitud de Datos 
+int_Numero = int(input('>>> Introduce un numero: '))
+
+if int_Numero == (int_Numero // 3) * 3:
+    print("El numero %d es multiplo de 3." %(int_Numero))
+else:
+    print("El numero %d no es multiplo de 3." %(int_Numero))
