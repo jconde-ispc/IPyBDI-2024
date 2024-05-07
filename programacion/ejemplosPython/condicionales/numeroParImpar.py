fc6f4898760e370e2a03cdd74e5abe15438d9b0a
Julian <jconde@ac.unrc.edu.ar>
2024-05-07 18:00:05 -0300

    desde otra compu

--- programacion/ejemplosPython/condicionales/numeroParImpar.py
+++ programacion/ejemplosPython/condicionales/numeroParImpar.py
@@ -1,1 +1,12 @@
 # https://colab.research.google.com/drive/1FhbSGiIu7ZdBrmrmx6nHyGBdPZb35PuQ?usp=sharing
+
+# Declaracion de variables:
+int_Numero = 0
+
+# Solicitud de Datos
+int_Numero = int(input('>>> Introduce un numero: '))
+
+if int_Numero == (int_Numero // 2) * 2:
+    print(f"El numero {int_Numero} es par.")
+else:
+    print(f"El numero {int_Numero} es impar.")
