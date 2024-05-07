29ff02286de4ff126550c9090215a1bb51ae3a96
jconde-ispc <julianconde.ispc@gmail.com>
2024-05-07 15:16:51 -0300

    guardando primer ejercicio de clase

---
+++ programacion/pythonEnClases/promedio.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Ingrese la nota 1"))
+nota2 = float(input("Ingrese la nota 2"))
+
+promedio = (nota1 + nota2) / 2
+
+print(f"El promedio de las notas {nota1*2} y {nota2} es {promedio}")
+print("El promedio de las notas es",promedio)
